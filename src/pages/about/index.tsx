de1c3a1d137f6fe114def7718f216426532995ed
toppy <theerut@shoplive.dev>
2022-09-14 02:24:23 +0700

    Updated content

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -31,20 +31,19 @@ const About: NextPage = () => {
             Ajarn &quot;PRO&quot; Electrical Services, Inc. (APES) is a control
             systems integration company offering expertise in custom automation
             and control systems solutions. We provide automation services from
-            conception through completion on a fixed price or T&M basis. AES can
-            design and develop a wide range of control systems utilizing any
+            conception through completion on a fixed price or T&M basis. APES
+            can design and develop a wide range of control systems utilizing any
             style of PLCs.
           </p>
           <p className='text-xs mt-8 md:text-sm lg:text-base'>
             Since its establishment in 2020, Ajarn &quot;PRO&quot; Electrical
             Services Company has grown to become one of Bangkok&apos;s largest
-            electrical contractors with more than 90 years of successful
-            service. Ajarn &quot;PRO&quot; Electric manifests its commitment to
-            quality through its dedicated employees. Over 500 construction
-            personnel, field electricians, professional engineers, project
-            managers, designers, industrial automation professionals,
-            management, and clerical staff are committed to making your project
-            successful.
+            electrical contractors with more than 2 years of successful service.
+            Ajarn &quot;PRO&quot; Electric manifests its commitment to quality
+            through its dedicated employees. Over 50 construction personnel,
+            field electricians, professional engineers, project managers,
+            designers, industrial automation professionals, management, and
+            clerical staff are committed to making your project successful.
           </p>
         </section>
       </main>
